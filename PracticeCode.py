f45979a9779319e54ce6b266f274e44790f3406e
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-27 10:24:31 -0400

    Update PracticeCode.py

--- PracticeCode.py
+++ PracticeCode.py
@@ -35,3 +35,19 @@ df1 = pd.DataFrame([
     columns = ['Country', 'Capital', 'Population']
                   )
 print(df1)
+
+# Another sample
+
+f2 = pd.DataFrame([
+  [1, 'San Diego', 100],
+  [2, 'Los Angeles', 120],
+  [3, 'San Francisco', 90],
+  [4, 'Sacramento', 115]
+],
+  columns=[
+   'Store ID',
+   'Location',
+   'Number of Employees'
+  ])
+
+print(df2)
