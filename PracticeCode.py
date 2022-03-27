19a9be452a1f8ae102d137c2d5b478b63e856beb
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-27 08:47:06 -0400

    Create PracticeCode.py

---
+++ PracticeCode.py
@@ -0,0 +1,8 @@
+# 1. Creating a new dataframe:
+
+df = pd.DataFrame({
+    'name': ['Kate', 'John', 'Travis'], 
+    'age': [32, 34, 36], 
+    'occupation': ['Physician', 'Engineer', 'Clerk']
+    })
+print(df)
