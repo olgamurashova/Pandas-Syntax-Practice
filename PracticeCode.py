d2d555d7292e4367cf712c129abd50571195b6e9
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-27 09:04:21 -0400

    Update PracticeCode.py

--- PracticeCode.py
+++ PracticeCode.py
@@ -6,3 +6,12 @@ df = pd.DataFrame({
     'occupation': ['Physician', 'Engineer', 'Clerk']
     })
 print(df)
+
+# 2. Creating a new dataframe sample:
+
+df1 = pd.DataFrame({
+  'Product ID': [1, 2, 3, 4],
+  'Product Name': ['t-shirt', 't-shirt', 'skirt', 'skirt'],
+  'Color': ['blue', 'green', 'red', 'black']
+})
+print(df1)
