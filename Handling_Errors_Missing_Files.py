9e8cb820c481eca88048e9b09ffba9bc8ec2aa41
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-31 13:46:18 -0400

    Update Handling_Errors_Missing_Files.py

--- Handling_Errors_Missing_Files.py
+++ Handling_Errors_Missing_Files.py
@@ -1,1 +1,16 @@
+# 1. Cheking data types:
 
+print('tablename.csv'.dtypes)
+
+# 2. Specifying data types:
+
+df = pd.read_csv('tablename.csv', dtype = {'zipcode' : str})
+
+# 3. Setting custom missing values using na_values keyword:
+
+df = pd.read_csv('tablename.csv',
+                 na_values = {'zipcode': 0})
+
+#Filterining NaN values in a column by using .isna() function:
+
+print(df[df.zipcode.isna()])
