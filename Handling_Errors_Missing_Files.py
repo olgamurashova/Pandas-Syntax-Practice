055189448e7c0472952bd246630199bc95af3175
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-31 14:10:34 -0400

    Update Handling_Errors_Missing_Files.py

--- Handling_Errors_Missing_Files.py
+++ Handling_Errors_Missing_Files.py
@@ -32,3 +32,15 @@ data = pd.read_csv("vt_tax_data_2016.csv", dtype=data_types)
 
 # Print data types of resulting frame
 print(data.dtypes.head())
+
+# 6. Setting custon NaN values:
+
+# Create dict specifying that 0s in zipcode are NA values
+null_values = {'zipcode' : 0}
+
+# Load csv using na_values keyword argument
+data = pd.read_csv("vt_tax_data_2016.csv", 
+                   na_values = null_values)
+
+# View rows with NA ZIP codes
+print(data[data.zipcode.isna()])
