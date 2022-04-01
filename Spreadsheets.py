ffcf9430b8a7c8ef9c78c5c25e36ea0f9aa313a1
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-01 12:00:28 -0400

    Update Spreadsheets.py

--- Spreadsheets.py
+++ Spreadsheets.py
@@ -10,4 +10,15 @@ excel_df = pd.read_excel('tablename.xlsx',
                          ncols = "W:AB, AR")
 print(excel_df.head())
 
-# 3. 
+# 3. read the excel file:
+
+# Load pandas as pd
+import pandas as pd
+
+# Read spreadsheet and assign it to survey_responses
+survey_responses = pd.read_excel('fcc_survey.xlsx')
+
+# View the head of the dataframe
+print(survey_responses.head())
+
+# 4. 
