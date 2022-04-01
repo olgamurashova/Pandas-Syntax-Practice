a8c104f6d1281f0c528b27b72f43d09b16a4b1ad
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-01 11:52:21 -0400

    Update Spreadsheets.py

--- Spreadsheets.py
+++ Spreadsheets.py
@@ -1,3 +1,13 @@
 # 1. Read the excel file:
 
 df = pd.read_excel('tablename.xlsx')
+print(df.head())
+
+# 2. Loading select columns and rows:
+
+excel_df = pd.read_excel('tablename.xlsx',
+                         skiprows = 2,
+                         ncols = "W:AB, AR")
+print(excel_df.head())
+
+# 3. 
