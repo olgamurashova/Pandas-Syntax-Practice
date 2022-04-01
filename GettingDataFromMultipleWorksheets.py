aed6162336ad41ba1f1596865967c706fbd4e550
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-01 12:38:37 -0400

    Update GettingDataFromMultipleWorksheets.py

--- GettingDataFromMultipleWorksheets.py
+++ GettingDataFromMultipleWorksheets.py
@@ -13,5 +13,27 @@ excel_df = pd.read_excel('tablename.xlsx',
 
 print(type(excel_df)) # checking the type of data result
 
-# 3. 
+for key, value in excel.df.items():
+  print(key, type(value))
+
+# 3. Putting all spreadsheets together:
+
+# Create empty dataframe
+all_sheets_df = pd.DataFrame()
+
+# Iterate through dataframes in dictionary(after loaded all sheets they formed a dictionary)
+
+for sheet_name, frame in excel_df.items():
+  # Add a column for we know which year the data is from
+  frame['Year'] = sheet_name
+  
+# Add each dataframe to empty dataframe all_sheets_df
+
+all_sheets_df = all_sheets_df.append(frame)
+
+# View years in data:
+
+print(all_sheets_df.Year.unique())
+
+
 
