1bea1c4b65b09392925e83b5bd244536bf9b0fef
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-01 12:10:46 -0400

    Update Spreadsheets.py

--- Spreadsheets.py
+++ Spreadsheets.py
@@ -21,4 +21,15 @@ survey_responses = pd.read_excel('fcc_survey.xlsx')
 # View the head of the dataframe
 print(survey_responses.head())
 
-# 4. 
+# 4. # Create string of lettered columns to load
+col_string = "AD, AW:BA"
+
+# Load data with skiprows and usecols set
+survey_responses = pd.read_excel("fcc_survey_headers.xlsx", 
+                                 skiprows=2, 
+                                 usecols=col_string)
+
+# View the names of the columns selected
+print(survey_responses.columns)
+
+
