107cfa3b10d891a37f66f64e581e0d1b13c7d38a
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-03 03:46:01 -0400

    Update ModifyingImportsTrueFalseData.py

--- ModifyingImportsTrueFalseData.py
+++ ModifyingImportsTrueFalseData.py
@@ -8,3 +8,15 @@ print(df.dtypes)
 # Count booleans values:
 print(df.sum())
 
+# Count Nas/Checking how many values are missing in the table
+print(df.isna().sum())
+
+# Loading data/ Casting True/False columns as Boolean
+bool_data = pd.read_excel('tablename.xlsx',
+                          dtype = {
+                            'columnname':bool,
+                            'columnname':bool,
+                            'columnname':bool})
+
+
+# 2. 
