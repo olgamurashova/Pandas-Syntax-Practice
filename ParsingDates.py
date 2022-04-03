5ffd848776ecd78bbd30c9572df6dade180b18ec
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-03 05:52:48 -0400

    Update ParsingDates.py

--- ParsingDates.py
+++ ParsingDates.py
@@ -4,3 +4,17 @@
 date_cols = ['column1', 'column2']
 df = pd.read_excel('tablename.xlsx',
                    parse_dates = date_cols)
+
+# 2.Splitting datetime columns:
+
+# List columns of dates to parse:
+
+date_cols = ['column1',
+             'column2',
+             [['column3,
+              'column4]]]
+
+# Loading file, parsing standard and split datetime columns:
+               
+df = pd.read_excel('tablename.xlsx',
+                   parse_dates = date_cols)
