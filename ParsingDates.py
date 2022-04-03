292a9589128cc948deb71b4a8029d7a9a80ffa43
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-03 11:16:59 -0400

    Update ParsingDates.py

--- ParsingDates.py
+++ ParsingDates.py
@@ -53,3 +53,9 @@ survey_data = pd.read_excel("fcc_survey_dts.xlsx",
 
 # View summary statistics about Part2Start
 print(survey_data.Part2Start.describe())                            
+
+# 8. # Parse datetimes and assign result back to Part2EndTime
+survey_data["Part2EndTime"] = pd.to_datetime(survey_data["Part2EndTime"], 
+                                             format="%m%d%Y %H:%M:%S")
+# Print first few values of Part2EndTime
+print(survey_data.Part2EndTime.head())                            
