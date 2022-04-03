1f090e083c28dd98328651a6212ce500a26a3847
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-03 04:31:55 -0400

    Update ModifyingImportsTrueFalseData.py

--- ModifyingImportsTrueFalseData.py
+++ ModifyingImportsTrueFalseData.py
@@ -30,6 +30,13 @@ bool_data = pd.read_excel('tablename.xlsx',
                              'false_values' = ['No'])
 To avoid fame Na values being True, we should keep original value type (like float)
 
-# 3. 
+# 3. # Set dtype to load appropriate column(s) as Boolean data
+survey_data = pd.read_excel("fcc_survey_subset.xlsx",
+                            dtype= {'HasDebt': bool})
+
+# View financial burdens by Boolean group
+print(survey_data.groupby('HasDebt').sum())
+
+
 
 
