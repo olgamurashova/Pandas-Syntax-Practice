3ed87216ecbaaba1500217dd41d574ab40675432
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-03 05:40:30 -0400

    Update ParsingDates.py

--- ParsingDates.py
+++ ParsingDates.py
@@ -1,1 +1,6 @@
+# 1. To parse the dates we pass the names of the columns with dates we want to parse:
 
+# List columns of dates to parse
+date_cols = ['column1', 'column2']
+df = pd.read_excel('tablename.xlsx',
+                   parse_dates = date_cols)
