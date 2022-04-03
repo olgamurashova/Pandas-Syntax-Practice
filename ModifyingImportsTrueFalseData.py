4f3fcdb99e616950024b15c5010533d3363946f9
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-03 03:36:45 -0400

    Update ModifyingImportsTrueFalseData.py

--- ModifyingImportsTrueFalseData.py
+++ ModifyingImportsTrueFalseData.py
@@ -1,1 +1,10 @@
+# 1. Booleans True/False 
+
+# Import the table first
+
+df = pd.read_excel('tablename.xlsx')
+# Read data types to determine if boolean values have been recognized:
+print(df.dtypes)
+# Count booleans values:
+print(df.sum())
 
