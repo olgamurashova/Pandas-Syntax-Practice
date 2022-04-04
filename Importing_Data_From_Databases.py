7507547d73c97f8901cb94c91cd0c2dfcf5cab79
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-04 13:25:01 -0400

    Update Importing_Data_From_Databases.py

--- Importing_Data_From_Databases.py
+++ Importing_Data_From_Databases.py
@@ -66,3 +66,18 @@ safety_calls = pd.read_sql(query, engine)
 call_counts = safety_calls.groupby('borough').unique_key.count()
 call_counts.plot.barh()
 plt.show()
+
+# 6. # Create query for records with max temps <= 32 or snow >= 1
+query = """
+SELECT *
+  FROM weather
+  WHERE tmax <=32
+  OR
+  snow >=1;
+"""
+
+# Query database and assign result to wintry_days
+wintry_days = pd.read_sql(query, engine)
+
+# View summary stats about the temperatures
+print(wintry_days.describe())
