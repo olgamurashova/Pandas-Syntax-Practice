05d48c0c192eb98c0b9b2f959b9a000af8a74b32
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-04 11:06:42 -0400

    Update Importing_Data_From_Databases.py

--- Importing_Data_From_Databases.py
+++ Importing_Data_From_Databases.py
@@ -20,4 +20,17 @@ hpd_calls = pd.read_sql("hpd311calls", engine)
 # View the first few rows of data
 print(hpd_calls.head())
 
-# 3. 
+# 3. # Create the database engine
+engine = create_engine("sqlite:///data.db")
+
+# Create a SQL query to load the entire weather table
+query = """
+SELECT * 
+  FROM weather;
+"""
+
+# Load weather with the SQL query
+weather = pd.read_sql("weather", engine)
+
+# View the first few rows of data
+print(weather.head())
