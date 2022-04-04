8b63befe751522d6b06da5e60b824d70a4b7543f
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-04 12:27:45 -0400

    Update Importing_Data_From_Databases.py

--- Importing_Data_From_Databases.py
+++ Importing_Data_From_Databases.py
@@ -34,3 +34,20 @@ weather = pd.read_sql("weather", engine)
 
 # View the first few rows of data
 print(weather.head())
+
+# 4. # Create database engine for data.db
+engine = create_engine('sqlite:///data.db')
+
+# Write query to get date, tmax, and tmin from weather
+query = """
+SELECT date, 
+       tmax, 
+       tmin
+  FROM weather;
+"""
+
+# Make a dataframe by passing query and engine to read_sql()
+temperatures = pd.read_sql(query, engine)
+
+# View the resulting dataframe
+print(temperatures)
