b35c571e655744081844d322f38cb6b1627bbf71
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-06 15:34:46 -0400

    Update Importing_Json_Data.py

--- Importing_Json_Data.py
+++ Importing_Json_Data.py
@@ -1,3 +1,4 @@
+# 1. 
 # Load pandas as pd
 import pandas as pd
 
@@ -6,5 +7,20 @@ pop_in_shelters = pd.read_json('dhs_daily_report.json')
 
 # View summary stats about pop_in_shelters
 print(pop_in_shelters.describe())
+
+# 2. 
+try:
+    # Load the JSON with orient specified
+    df = pd.read_json("dhs_report_reformatted.json",
+                      "split")
+    
+    # Plot total population in shelters over time
+    df["date_of_census"] = pd.to_datetime(df["date_of_census"])
+    df.plot(x="date_of_census", 
+            y="total_individuals_in_shelter")
+    plt.show()
+    
+except ValueError:
+    print("pandas could not parse the JSON.")
 
 
