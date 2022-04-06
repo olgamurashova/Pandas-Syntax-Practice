0fd72d877a870042dc24d81237146e24e384fac6
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-06 13:00:00 -0400

    Update Importing_Data_From_Databases.py

--- Importing_Data_From_Databases.py
+++ Importing_Data_From_Databases.py
@@ -187,4 +187,18 @@ leak_calls = pd.read_sql(query, engine)
 # View the dataframe
 print(leak_calls.head())
 
-#14. 
+#14. # Query to get water leak calls and daily precipitation
+query = """
+SELECT hpd311calls.*, weather.prcp
+  FROM hpd311calls
+  JOIN weather
+    ON hpd311calls.created_date = weather.date
+  WHERE hpd311calls.complaint_type = 'WATER LEAK';"""
+
+# Load query results into the leak_calls dataframe
+leak_calls = pd.read_sql(query, engine)
+
+# View the dataframe
+print(leak_calls.head())
+
+# 15. 
