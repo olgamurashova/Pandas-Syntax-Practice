6ba38300d6dab566b8cc8c66fe298454ee27f16c
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-07 03:25:26 -0400

    Update APIs.py

--- APIs.py
+++ APIs.py
@@ -14,3 +14,20 @@ cafes = pd.DataFrame(data["businesses"])
 
 # View the data's dtypes
 print(cafes.dtypes)
+
+# 2
+# Create dictionary to query API for cafes in NYC
+parameters = {"term": "cafe",
+          	  "location": "NYC" }
+
+# Query the Yelp API with headers and params set
+response = requests.get(api_url,
+                params = parameters,
+                headers = headers)
+
+# Extract JSON data from response
+data = response.json()
+
+# Load "businesses" values to a dataframe and print head
+cafes = pd.DataFrame(data["businesses"])
+print(cafes.head())
