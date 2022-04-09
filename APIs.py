0d489d795bb3b63e00f4ff1d267fafb2fcee67c7
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-04-09 03:54:46 -0400

    Update APIs.py

--- APIs.py
+++ APIs.py
@@ -66,3 +66,21 @@ cafes = json_normalize(data["businesses"],
 # View data
 print(cafes.head())
 
+Handle deeply nested data
+
+# 1.  
+
+# Load other business attributes and set meta prefix
+flat_cafes = json_normalize(data["businesses"],
+                            sep="_",
+                    		record_path="categories",
+                    		meta=["name", 
+                                  "alias",  
+                                  "rating",
+                          		  ["coordinates", "latitude"], 
+                          		  ["coordinates","longitude"]],
+                    		meta_prefix="biz_")
+
+# View the data
+print(flat_cafes.head())
+
